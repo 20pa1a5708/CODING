4f461444ee7ab0599bdf491180a79ccc1c2e4b24
20pa1a5708 <113027359+20pa1a5708@users.noreply.github.com>
2023-02-09 12:08:13 +0530

    Create Subarray Division1.java

---
+++ Subarray Division1.java
@@ -0,0 +1,16 @@
+public static int birthday(List<Integer> s, int d, int m) {
+    // Write your code here
+    int result=0;
+for(int i=0;i<=s.size()-m;i++){
+    int sum =0;
+    for (int j=i;j<i+m;j++){
+        sum=sum+s.get(j);
+    }
+    if(sum==d){
+    result++;
+    }
+}
+return result;
+    }
+
+}
