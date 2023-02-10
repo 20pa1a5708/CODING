3313f767f03ad019a903f283ec113aba16679aff
20pa1a5708 <113027359+20pa1a5708@users.noreply.github.com>
2023-02-10 22:19:12 +0530

    Create delete linked list.java

---
+++ delete linked list.java
@@ -0,0 +1,8 @@
+class Solution {
+    public void deleteNode(ListNode node) {
+        node.val=node.next.val;
+      ListNode next=node.next.next;
+    ListNode prev=node;
+    prev.next=next;
+}
+}
