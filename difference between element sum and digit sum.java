c89bfcd16c37c32f664793132396dac1db863eea
20pa1a5708 <113027359+20pa1a5708@users.noreply.github.com>
2023-02-10 21:50:49 +0530

    Create difference between element sum and digit sum.java

---
+++ difference between element sum and digit sum.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int differenceOfSum(int[] nums) {
+        int sum=0;
+        int sum1=0;
+for(int num:nums){
+           sum+=num;
+           while(num>0){
+               sum1+=num%10;
+               num/=10;
+           }
+}
+       return Math.abs(sum-sum1);
+    }
+}
